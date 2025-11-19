797477ae8439539b1e46a8dfb55bd2fdcd8b0ae8
JuTerrr <julie@terryn.org>
2025-11-19 17:08:54 +0100

    Left Menu + new pages

--- Chess_DB/ViewModels/MainWindowViewModel.cs
+++ Chess_DB/ViewModels/MainWindowViewModel.cs
@@ -4,17 +4,27 @@ using System.IO;
 using System;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Chess_DB.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private bool _isPaneOpen = true;
+    [ObservableProperty]
+    private ViewModelBase _currentPage = new HomePageViewModel();
+    [RelayCommand]
+    private void TriggerPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
 
     public MainWindowViewModel()
     {
         Console.WriteLine("Starting database test...");
 
-        
+
     }
 }
---
+++ Chess_DB/Views/HomePageView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Chess_DB.ViewModels;
+
+namespace Chess_DB.Views;
+
+public partial class HomePageView : UserControl
+{
+    public HomePageView()
+    {
+        InitializeComponent();
+    }
+}
