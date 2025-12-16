e690f9b94a5eb21de00bd3f829374e24a442f92a
Haaplix 0.5 <Adamalihussain1306@gmail.com>
2025-12-16 20:40:03 +0100

    resolve merge bug

--- Chess_DB/ViewModels/PlayersPageViewModel.cs
+++ Chess_DB/ViewModels/PlayersPageViewModel.cs
@@ -1,7 +1,15 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging;
+using Chess_DB.Messages;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Collections.ObjectModel;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Chess_DB.ViewModels;
 
