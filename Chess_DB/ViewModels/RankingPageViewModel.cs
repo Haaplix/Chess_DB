d3114411b139d787f9346bb00f77d9456c36b514
JuTerrr <julie@terryn.org>
2025-12-16 19:44:42 +0100

    Add Ranking Page

---
+++ Chess_DB/ViewModels/RankingPageViewModel.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging;
+using Chess_DB.Messages;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Chess_DB.ViewModels;
+
+public partial class RankingPageViewModel : PlayersPageViewModel
+{
+
+}
+
+
---
+++ Chess_DB/Views/RankingPageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Chess_DB.Views;
+
+public partial class RankingPageView : UserControl
+{
+    public RankingPageView()
+    {
+        InitializeComponent();
+    }
+}
