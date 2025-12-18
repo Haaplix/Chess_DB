fc1aa1c1d5d3bfe02c19d54d6215caf6ea7b543c
Haaplix <Adamalihussain1306@gmail.com>
2025-12-18 18:36:26 +0100

    Ranking by elo is done

--- Chess_DB/ViewModels/RankingPageViewModel.cs
+++ Chess_DB/ViewModels/RankingPageViewModel.cs
@@ -10,12 +10,33 @@ using System.Data;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.Linq;
+using SQLitePCL;
+using System.Diagnostics.CodeAnalysis;
 
 
 namespace Chess_DB.ViewModels;
 
 public partial class RankingPageViewModel : PlayersPageViewModel
 {
+    public RankingPageViewModel()
+    {
+        _ = RankingElo();
+    }
+
+    [ObservableProperty]
+    private ObservableCollection<LightPlayerViewModel> playerEloRankList = new();
+    private async Task RankingElo()
+    {
+        using (var context = new AppDbContext())
+        {
+            var players = await context.Players.OrderByDescending(p => p.ELO).ToListAsync();
+            PlayerEloRankList.Clear();
+            foreach (var player in players)
+            {
+                PlayerEloRankList.Add(new LightPlayerViewModel(player));
+            }
+        }
+    }
 
 }
 
